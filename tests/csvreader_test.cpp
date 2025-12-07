2836164a02db8f11f2f931b8d416589cd940006b
bsobocki <bartq98@gmail.com>
2025-12-08 00:04:59 +0100

    add test case for quoting headers

--- tests/csvreader_test.cpp
+++ tests/csvreader_test.cpp
@@ -5,10 +5,19 @@ TEST(CsvReaderTest, OpenTheFile) {
     EXPECT_NO_THROW(CsvReader reader("./test_data/simple_file.csv"));
 }
 
-TEST(CsvReaderTest, ReadHeaders) {
+TEST(CsvReaderTest, ReadSimpleHeaders) {
     CsvReader reader("./test_data/simple_file.csv");
     EXPECT_TRUE(reader.has_header());
 
     std::vector<std::string> expectedHeaders = {"name" , "age", "country"};
+    EXPECT_EQ(reader.headers(), expectedHeaders);
+}
+
+TEST(CsvReaderTest, ReadQuotingHeaders) {
+    CsvReader reader("./test_data/quoting.csv");
+    EXPECT_TRUE(reader.has_header());
+
+    std::vector<std::string> expectedHeaders = {"Product", "Description", "Price, but Netto"};
+    // for naive implementation the result is { "\"Product\"", "\"Description\"", "\"Price", " but Netto\"" }
     EXPECT_EQ(reader.headers(), expectedHeaders);
 }
