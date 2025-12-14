9a0dc85163761b2e498a54c7c818b55d4c06573f
bsobocki <bartq98@gmail.com>
2025-12-14 17:48:36 +0100

    update config

--- engine/inc/csvconfig.hpp
+++ engine/inc/csvconfig.hpp
@@ -5,10 +5,10 @@ namespace csv {
 struct Config {
     char delimiter = ',';
     bool has_header = true;
-    
-    enum class ErrorHandlerMode { strict, lenient };
-    ErrorHandlerMode error_handler_mode = ErrorHandlerMode::strict;
-    
+
+    enum class ParseMode { strict, lenient };
+    ParseMode parse_mode = ParseMode::strict;
+
     enum class LineEnding { Auto, LF, CRLF, CR };
     LineEnding line_ending = LineEnding::Auto;
 
