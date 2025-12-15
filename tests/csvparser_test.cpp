7c00b340be2ec82d1e4792f8dc0919d1ecb0d46c
bsobocki <bartq98@gmail.com>
2025-12-15 11:51:55 +0100

    introduce parser tests

--- tests/csvparser_test.cpp
+++ tests/csvparser_test.cpp
@@ -6,19 +6,22 @@
 
 using namespace csv;
 
-TEST(ParserTest, ) {
-    
-
-}
-
 TEST(ParserTest, StrictParsing_Malformed_QuoteInUnquotedField) {
     std::string input = R"(aa"ada","normal")";
     Parser parser();
-    EXPECT_EQ(parse(input), Parser::ParseStatus::fail);
+    EXPECT_EQ(parser.parse(input), Parser::ParseStatus::fail);
 }
 
 TEST(ParserTest, StrictParsing_Malformed_ContentAfterClosingQuote) {
     std::string input = R"("something""different"here,next)";
     Parser parser();
-    EXPECT_EQ(parse(input), Parser::ParseStatus::fail);
+    EXPECT_EQ(parser.parse(input), Parser::ParseStatus::fail);
+}
+
+TEST(ParserTest, StrictParsing_CorrectQuoting_NoContentAfterClosingQuote) {
+    std::string input = R"("something""different",next)";
+    Parser parser();
+    std::vector<std::string> expectedFields = {"something""different", "next"};
+    EXPECT_EQ(parser.parse(input), Parser::ParseStatus::complete);
+    EXPECT_EQ(parser.fields(), expectedFields);
 }
