34febc2d156adc14260c7578144d36ef93784129
bsobocki <bartq98@gmail.com>
2026-01-02 00:44:30 +0100

    improve errors msg info

--- engine/inc/csverrors.hpp
+++ engine/inc/csverrors.hpp
@@ -21,16 +21,26 @@ public:
     FileStreamError(): std::runtime_error("Stream failed") {}
     FileStreamError(const std::string& filename): std::runtime_error("Stream failed for file: " + filename) {}
     FileStreamError(const std::string_view& filename): std::runtime_error("Stream failed for file: " + std::string(filename)) {}
+    FileStreamError(const std::string& filename, size_t line_number)
+        : std::runtime_error("Stream failed for file: " + filename  + " at line " + std::to_string(line_number)) {}
+    FileStreamError(const std::string_view& filename, size_t line_number)
+        : std::runtime_error("Stream failed for file: " + std::string(filename) + " at line " + std::to_string(line_number)) {}
 };
 
 class RecordDataError : public std::runtime_error {
 public:
     RecordDataError(): std::runtime_error("Cannot read record.") {}
+    RecordDataError(size_t line_number): std::runtime_error("Cannot read record at line " + std::to_string(line_number)) {}
 };
 
 class RecordSizeError : public std::runtime_error {
 public:
     RecordSizeError(): std::runtime_error("Unexpected record size.") {}
+    RecordSizeError(size_t line_number, size_t expected_size, size_t actual_size)
+        : std::runtime_error(
+            "Record size mismatch: expected " + std::to_string(expected_size) +
+            ", got " + std::to_string(actual_size) +
+            " at line " + std::to_string(line_number)) {}
 };
 
 }
