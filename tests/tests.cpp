7b96deac6aebf5a1f477bf78a2d6a36ab5e9f818
bsobocki <bartq98@gmail.com>
2026-01-08 02:03:08 +0100

    move tests source to tests/src

--- tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// Because of our CMake setup, we can include the header directly
-#include <csvreader.hpp>
-
-// Basic test to show how it works
-TEST(GeneralTests, AlwaysPasses) {
-    ASSERT_TRUE(true);
-}
