3b082c65a35bbf0f6a2d841fdc7e2948110286a6
bsobocki <bartq98@gmail.com>
2026-02-05 23:27:53 +0100

    init csvparser simd parser class without implementation

--- engine/inc/csvparser.hpp
+++ engine/inc/csvparser.hpp
@@ -52,6 +52,16 @@ private:
 
 
 
+class SimdParser : public Parser {
+public:
+    explicit SimdParser(const Config& config);
+
+    /// @brief function parse doesn't reset the parser state
+    [[nodiscard]] ParseStatus parse(std::string_view buffer) override;
+};
+
+
+
 class QuotingParser : public Parser {
 public:
     explicit QuotingParser(const Config& config);
